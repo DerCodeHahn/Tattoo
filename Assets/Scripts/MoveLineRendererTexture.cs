20456a9b147888f4c5232dfa29be070bf15b9ff7
HahnC <christopher.hahn@edurino.com>
2024-07-14 14:48:47 +0200

    fixed build issues

--- Assets/Scripts/MoveLineRendererTexture.cs
+++ Assets/Scripts/MoveLineRendererTexture.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TreeEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
@@ -12,17 +11,14 @@ public class MoveLineRendererTexture : MonoBehaviour
 
     
     float offset = 0;
-    private FractalNoise noise;
     void Start()
     {
-        noise = new FractalNoise(1.0f, 1.0f, 8.0f);
-        
         lineRenderer = GetComponent<LineRenderer>();
     }
 
     void Update()
     {
-        offset += speed * Time.deltaTime * (1+noise.BrownianMotion(0, Time.time * noiseSpeed));
+        offset += speed * Time.deltaTime * (1+Mathf.PerlinNoise1D(Time.time * noiseSpeed));
         lineRenderer.sharedMaterial.SetFloat("_Offset",offset);
     }
 }
